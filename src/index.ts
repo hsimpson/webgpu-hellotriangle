419490b9b7d2bb044a0802b43d37450894f3542c
Daniel Toplak <homer.simpson123@gmx.de>
2020-06-04 01:55:27 +0200

    implement canvas resizing

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,4 @@ import WebGPURenderer from './webgpurenderer';
 const canvas: HTMLCanvasElement = document.getElementById('webgpu_canvas') as HTMLCanvasElement;
 
 const renderer = new WebGPURenderer(canvas);
-console.log(`canvas size: ${canvas.width}, ${canvas.height}`);
 renderer.start();
--- src/webgpurenderer.ts
+++ src/webgpurenderer.ts
@@ -1,11 +1,37 @@
+import ResizeObserver from 'resize-observer-polyfill';
+
 export default class WebGPURenderer {
+  private canvas: HTMLCanvasElement;
+
   public constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+
     // set canvas size to css size * device pixel ratio
-    canvas.width = canvas.offsetWidth * window.devicePixelRatio;
-    canvas.height = canvas.offsetHeight * window.devicePixelRatio;
+    //this.canvas.width = canvas.offsetWidth * window.devicePixelRatio;
+    //this.canvas.height = canvas.offsetHeight * window.devicePixelRatio;
+  }
+
+  private resize(width: number, height: number): void {
+    this.canvas.width = width;
+    this.canvas.height = height;
+    console.log(`canvas size: ${this.canvas.width}, ${this.canvas.height}`);
+  }
+
+  private initialize(): void {
+    const ro = new ResizeObserver((entries) => {
+      if (!Array.isArray(entries)) {
+        return;
+      }
+      this.resize(
+        entries[0].contentRect.width * window.devicePixelRatio,
+        entries[0].contentRect.height * window.devicePixelRatio
+      );
+    });
+
+    ro.observe(this.canvas);
   }
 
   public start(): void {
-    // do nothing yet
+    this.initialize();
   }
 }
