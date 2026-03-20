f0cfa4a46693e76c6d3e9fc7433d5c05ef965b11
Daniel Toplak <homer.simpson123@gmx.de>
2026-03-20 23:11:44 +0100

    feat: update packages

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,44 +3,49 @@ import tseslint from 'typescript-eslint';
 
 import configPrettier from 'eslint-config-prettier';
 import pluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import { defineConfig } from 'eslint/config';
 
-export default tseslint.config(
+export default defineConfig(
   eslint.configs.recommended,
   tseslint.configs.strictTypeChecked,
-  // ...tseslint.config.stylistic,
+  tseslint.configs.stylisticTypeChecked,
 
   {
-    ignores: ['dist/**/*', 'eslint.config.mjs', '.prettierrc.js', 'vite.config.mjs'],
+    ignores: ['coverage/**/*', 'dist/**/*'],
   },
+
+  // configuration for rules that require type information
   {
     languageOptions: {
-      parserOptions: { projectService: true, tsconfigRootDir: import.meta.dirname },
+      parserOptions: {
+        projectService: true,
+      },
     },
   },
 
-  // eslint rules
+  // customize eslint-rules
   {
     rules: {
-      'linebreak-style': ['error', 'unix'],
-      'no-unused-vars': 'off',
+      'no-unused-vars': 'off', // Note: you must disable the base rule as it can report incorrect errors
       'no-warning-comments': 'warn',
       eqeqeq: 'error',
-      indent: ['error', 2, { SwitchCase: 1 }],
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
     },
   },
 
+  // customize typescript-eslint rules
   {
     rules: {
+      '@typescript-eslint/consistent-type-definitions': 'off',
       '@typescript-eslint/explicit-member-accessibility': ['error', { accessibility: 'explicit' }],
-      '@typescript-eslint/interface-name-prefix': 'off',
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/no-parameter-properties': ['off'],
-      '@typescript-eslint/no-require-imports': 'warn',
+      '@typescript-eslint/no-shadow': 'error',
       '@typescript-eslint/no-unused-vars': [
         'warn',
-        { args: 'after-used', argsIgnorePattern: '^_', ignoreRestSiblings: false, vars: 'all' },
+        {
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          ignoreRestSiblings: false,
+          vars: 'all',
+        },
       ],
       '@typescript-eslint/no-var-requires': 'warn',
       '@typescript-eslint/promise-function-async': 'error',
--- prettier.config.ts
+++ prettier.config.ts
@@ -1,6 +1,10 @@
-module.exports = {
+import { type Config } from 'prettier';
+
+const config: Config = {
   arrowParens: 'avoid',
   printWidth: 100,
   singleQuote: true,
   trailingComma: 'all',
 };
+
+export default config;
